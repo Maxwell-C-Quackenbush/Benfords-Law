f82a560cbca8b253acb917405035134f4fae96cf
JP Juteau <jp@rocklandscientific.com>
2022-11-07 13:03:26 -0800

    fix some issues

--- utilities.py
+++ utilities.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 # gets the n-th digit of a given number. 
 # e.g. returns 3 for: 3456.78, 1
@@ -7,17 +9,21 @@ import numpy as np
 # if the requested digit is larger than the number
 # e.g. requesting the 2nd digit of 5, the function returns NaN
 def get_digit(number, nth=1):
+    number = np.abs(number)
+    
+    if number < 1:
+        return 0
     order = math.floor( math.log10(number) );
 
     if (nth > order):
-        return math.nan
+        return 0
 
     num_str = str(number);
     return int( num_str[nth-1] )
 
 # Extract the distribution associated
 def benfor_distribution():
-    Pd = []
+    Pd = np.zeros(9)
     for i in range(9):
         Pd[i] = math.log10(1 + 1/(i+1))
     return Pd
@@ -26,17 +32,41 @@ def benfor_distribution():
 
 def test_gaussian():
     # Generate 1000 Gaussian distributed points (around 0 with normal distribution of 10) 
-    X = np.random.normal(0,10,1000)
-    dist = np.array(9)
+    X = np.random.normal(0,1000,1000)
+    dist = np.zeros(10)
 
     for i in range(len(X)):
         dig = get_digit(X[i])
         dist[dig] += 1
 
-    return dist / len(X)
+    return dist[1:10] / (len(X)-dist[0])
 
 
 
+
+def main():
+    
+    benford_dist = benfor_distribution()
+    gaussian_dist = test_gaussian()
+    
+    idx = np.arange(1,10)
+    
+    plt.rc('lines', linewidth=2.5)
+    fig, ax = plt.subplots()
+    
+    ax.plot(idx, benford_dist, label="Benford distribution")
+    ax.plot(idx, gaussian_dist, label="Gaussian distribution")
+    
+    ax.legend(handlelength=2)
+    plt.show()
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
 
 
 
